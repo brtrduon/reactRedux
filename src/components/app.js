574c83a325dac0e39d3716f4746b55458619dd29
unknown <you@example.com>
2018-02-17 17:57:39 -0800

    removed readme file

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 // import { Component } from 'react';
 
+// don't need line 2 because line 1 already covers for { component }
+
 import BookList from '../containers/book-list';
 import BookDetail from '../containers/book-detail';
 
